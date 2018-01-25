281731866a9f30a5e83bda267506e1895de0505b
Murugan <murugu2012@gmail.com>
2018-01-25 12:21:04 +0530

    Fix hound ci issue

--- lib/generators/draft/scaffold/scaffold_erb_generator.rb
+++ lib/generators/draft/scaffold/scaffold_erb_generator.rb
@@ -6,7 +6,6 @@ module Draft
   class ScaffoldErbGenerator < Erb::Generators::ScaffoldGenerator
     source_root File.expand_path("../templates", __FILE__)
 
-
     def copy_view_files
       available_views.each do |view|
         template view, File.join("app/views", controller_file_path, view)
@@ -18,7 +17,6 @@ module Draft
     def available_views
       base = self.class.source_root
       base_len = base.length + 1
-      
       Dir[File.join(base, "**", "*")].
         select { |f| File.file?(f) }.
         map { |f| f[base_len..-1] }
