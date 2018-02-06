c8066a2fddd781b1a7e456c495198cbb9b9d2d65
Murugan <murugu2012@gmail.com>
2018-02-06 10:13:30 +0530

    Trigger codeclimate

--- lib/generators/draft/model/model_generator.rb
+++ lib/generators/draft/model/model_generator.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails/generators/named_base"
-
 module Draft
   class ModelGenerator < Rails::Generators::NamedBase
     argument :attributes, type: :array, default: [],
