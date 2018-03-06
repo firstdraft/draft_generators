ad7445c10b2af2febb6238f02cd7ca34b41e8dd5
Murugan <murugu2012@gmail.com>
2018-03-06 14:05:11 +0530

    Do not generate assets in draft:scaffold

--- lib/generators/draft/scaffold/scaffold_generator.rb
+++ lib/generators/draft/scaffold/scaffold_generator.rb
@@ -6,6 +6,7 @@ require "rails/generators/rails/scaffold/scaffold_generator"
 module Draft
   class ScaffoldGenerator < ::Rails::Generators::ScaffoldGenerator
     remove_hook_for :scaffold_controller
+    remove_hook_for :assets
 
     def generate_controller
       invoke Draft::ScaffoldControllerGenerator
