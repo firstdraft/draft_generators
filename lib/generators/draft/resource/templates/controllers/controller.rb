fc07db027b20998d4dd7d6b41258484468dea72a
Raghu Betina <raghu@firstdraft.com>
2019-08-13 15:18:06 -0500

    Replace rt_ with route_
    Fix controller indentation
    
    Fix index form indentation
    
    Fix show page form
    
    Make the new form narrower
    
    Order index by created at descending
    
    Add margin-bottom to custom-checkboxes

--- lib/generators/draft/resource/templates/controllers/controller.rb
+++ lib/generators/draft/resource/templates/controllers/controller.rb
@@ -1,6 +1,6 @@
 class <%= plural_table_name.camelize %>Controller < ApplicationController
   def index
-    @<%= plural_table_name.underscore %> = <%= class_name.singularize %>.all
+    @<%= plural_table_name.underscore %> = <%= class_name.singularize %>.all.order({ :created_at => :desc })
     
     respond_to do |format|
       format.json do
@@ -14,7 +14,7 @@ class <%= plural_table_name.camelize %>Controller < ApplicationController
   end
 
   def show
-    the_id = params.fetch(:rt_<%= singular_table_name %>_id)
+    the_id = params.fetch(:route_<%= singular_table_name %>_id)
     @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({:id => the_id }).first
 
     respond_to do |format|
@@ -73,11 +73,15 @@ class <%= plural_table_name.camelize %>Controller < ApplicationController
   end
 
   def update
-    the_id = params.fetch(:rt_<%= singular_table_name %>_id)
+    the_id = params.fetch(:route_<%= singular_table_name %>_id)
     @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where(:id => the_id).at(0)
 
 <% attributes.each do |attribute| -%>
+<% if attribute.field_type == :check_box -%>
+    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch(:<%= attribute.column_name %>, false)
+<% else %>
     @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch(:<%= attribute.column_name %>, @<%= singular_table_name.underscore %>.<%= attribute.column_name %>)
+<% end %>
 <% end -%>
 
 <% unless skip_validation_alerts? -%>
@@ -122,21 +126,21 @@ class <%= plural_table_name.camelize %>Controller < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch(:rt_<%= singular_table_name %>_id)
+    the_id = params.fetch(:route_<%= singular_table_name %>_id)
     @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({ :id => the_id }).first
 
     @<%= singular_table_name.underscore %>.destroy
 
 <% unless skip_validation_alerts? -%>
-  respond_to do |format|
-    format.json do
-      render({ :json => @<%= singular_table_name.underscore %>.as_json })
-    end
+    respond_to do |format|
+      format.json do
+        render({ :json => @<%= singular_table_name.underscore %>.as_json })
+      end
 
-    format.html do
-      redirect_to("/<%= @plural_table_name.underscore %>", {:notice => "<%= singular_table_name.humanize %> deleted successfully."})
+      format.html do
+        redirect_to("/<%= @plural_table_name.underscore %>", {:notice => "<%= singular_table_name.humanize %> deleted successfully."})
+      end
     end
-  end
 <% else -%>
 <% unless skip_redirect? -%>
     redirect_to("/<%= @plural_table_name.underscore %>")
