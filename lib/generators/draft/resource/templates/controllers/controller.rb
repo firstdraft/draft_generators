991e59216393297412ad89c5869a297f09fe56b0
Jelani Woods <jelani@firstdraft.com>
2019-11-19 11:29:52 -0600

    Update format of inputs, labels, and routes
    Remove rendering JSON
    
    tabfix
    
    Additional fixes
    
    Remove fallbacks
    
    All should be as it's supposed to
    
    Add spaces around notice
    
    Add spacing around notices, redirect to show page with failure alert

--- lib/generators/draft/resource/templates/controllers/controller.rb
+++ lib/generators/draft/resource/templates/controllers/controller.rb
@@ -1,63 +1,33 @@
 class <%= plural_table_name.camelize %>Controller < ApplicationController
   def index
     @<%= plural_table_name.underscore %> = <%= class_name.singularize %>.all.order({ :created_at => :desc })
-    
-    respond_to do |format|
-      format.json do
-        render({ :json => @<%= plural_table_name.underscore %>.as_json })
-      end
 
-      format.html do
-        render({ :template => "<%= plural_table_name.underscore %>/index.html.erb" })
-      end
-    end
+    render({ :template => "<%= plural_table_name.underscore %>/index.html.erb" })
   end
 
   def show
-    the_id = params.fetch(:route_<%= singular_table_name %>_id)
-    @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({:id => the_id }).first
+    the_id = params.fetch("id_from_path")
+    @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({:id => the_id }).at(0)
 
-    respond_to do |format|
-      format.json do
-        render({ :json => @<%= singular_table_name.underscore %>.as_json })
-      end
-
-      format.html do
-        render({ :template => "<%= plural_table_name.underscore %>/show.html.erb" })
-      end
-    end
+    render({ :template => "<%= plural_table_name.underscore %>/show.html.erb" })
   end
 
   def create
     @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.new
-
 <% attributes.each do |attribute| -%>
-    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch(:<%= attribute.column_name %>, nil)
+<% if attribute.field_type == :check_box -%>
+    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch("<%= attribute.column_name %>_from_query", false)
+<% else -%>
+    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch("<%= attribute.column_name %>_from_query")
+<% end -%>
 <% end -%>
 
 <% unless skip_validation_alerts? -%>
     if @<%= singular_table_name.underscore %>.valid?
       @<%= singular_table_name.underscore %>.save
-      respond_to do |format|
-        format.json do
-          render({ :json => @<%= singular_table_name.underscore %>.as_json })
-        end
-  
-        format.html do
-          redirect_to("/<%= plural_table_name.underscore %>", { :notice => "<%= singular_table_name.humanize %> created successfully."})
-        end
-      end
-
+      redirect_to("/<%= plural_table_name.underscore %>", { :notice => "<%= singular_table_name.humanize %> created successfully." })
     else
-      respond_to do |format|
-        format.json do
-          render({ :json => @<%= singular_table_name.underscore %>.as_json })
-        end
-  
-        format.html do
-          redirect_to("/<%= plural_table_name.underscore %>", { :notice => "<%= singular_table_name.humanize %> failed to create successfully."})
-        end
-      end
+      redirect_to("/<%= plural_table_name.underscore %>", { :notice => "<%= singular_table_name.humanize %> failed to create successfully." })
     end
 <% else -%>
     @<%= singular_table_name.underscore %>.save
@@ -73,40 +43,23 @@ class <%= plural_table_name.camelize %>Controller < ApplicationController
   end
 
   def update
-    the_id = params.fetch(:route_<%= singular_table_name %>_id)
-    @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where(:id => the_id).at(0)
+    the_id = params.fetch("id_from_path")
+    @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({ :id => the_id }).at(0)
 
 <% attributes.each do |attribute| -%>
 <% if attribute.field_type == :check_box -%>
-    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch(:<%= attribute.column_name %>, false)
-<% else %>
-    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch(:<%= attribute.column_name %>, @<%= singular_table_name.underscore %>.<%= attribute.column_name %>)
-<% end %>
+    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch("<%= attribute.column_name %>_from_query", false)
+<% else -%>
+    @<%= singular_table_name.underscore %>.<%= attribute.column_name %> = params.fetch("<%= attribute.column_name %>_from_query")
+<% end -%>
 <% end -%>
 
 <% unless skip_validation_alerts? -%>
     if @<%= singular_table_name.underscore %>.valid?
       @<%= singular_table_name.underscore %>.save
-      respond_to do |format|
-        format.json do
-          render({ :json => @<%= singular_table_name.underscore %>.as_json })
-        end
-  
-        format.html do
-          redirect_to("/<%= @plural_table_name.underscore %>/#{@<%= singular_table_name.underscore %>.id}", {:notice => "<%= singular_table_name.humanize %> updated successfully."})
-        end
-      end
+      redirect_to("/<%= @plural_table_name.underscore %>/#{@<%= singular_table_name.underscore %>.id}", { :notice => "<%= singular_table_name.humanize %> updated successfully."} )
     else
-      # render({:template => "/<%= @plural_table_name.underscore %>/edit_form_with_errors.html.erb"})
-      respond_to do |format|
-        format.json do
-          render({ :json => @<%= singular_table_name.underscore %>.as_json })
-        end
-  
-        format.html do
-          render({ :template => "<%= plural_table_name.underscore %>/show.html.erb" })
-        end
-      end
+      redirect_to("/<%= @plural_table_name.underscore %>/#{@<%= singular_table_name.underscore %>.id}", { :alert => "<%= singular_table_name.humanize %> failed to update successfully." })
     end
 <% else -%>
     @<%= singular_table_name.underscore %>.save
@@ -114,49 +67,26 @@ class <%= plural_table_name.camelize %>Controller < ApplicationController
 <% unless skip_redirect? -%>
     redirect_to("/<%= @plural_table_name.underscore %>/#{@<%= singular_table_name.underscore %>.id}")
 <% else -%>
-  respond_to do |format|
-    format.json do
-      render({ :json => @<%= singular_table_name.underscore %>.as_json })
-    end
-
-    format.html do
       render({ :template => "<%= plural_table_name.underscore %>/show.html.erb" })
-    end<% end -%>
+  <% end -%>
 <% end -%>
   end
 
   def destroy
-    the_id = params.fetch(:route_<%= singular_table_name %>_id)
-    @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({ :id => the_id }).first
+    the_id = params.fetch("id_from_path")
+    @<%= singular_table_name.underscore %> = <%= class_name.singularize %>.where({ :id => the_id }).at(0)
 
     @<%= singular_table_name.underscore %>.destroy
 
 <% unless skip_validation_alerts? -%>
-    respond_to do |format|
-      format.json do
-        render({ :json => @<%= singular_table_name.underscore %>.as_json })
-      end
-
-      format.html do
-        redirect_to("/<%= @plural_table_name.underscore %>", {:notice => "<%= singular_table_name.humanize %> deleted successfully."})
-      end
-    end
+    redirect_to("/<%= @plural_table_name.underscore %>", { :notice => "<%= singular_table_name.humanize %> deleted successfully."} )
 <% else -%>
 <% unless skip_redirect? -%>
     redirect_to("/<%= @plural_table_name.underscore %>")
 <% else -%>
     @remaining_count = <%= class_name.singularize %>.count
     
-    respond_to do |format|
-      format.json do
-        render({ :json => @<%= singular_table_name.underscore %>.as_json })
-      end
-
-      format.html do
-        redirect_to("/<%= @plural_table_name.underscore %>", {:notice => "<%= singular_table_name.humanize %> deleted successfully."})
-      end
-    end
-    render("<%= singular_table_name.underscore %>_templates/destroy_row.html.erb")
+    redirect_to("/<%= @plural_table_name.underscore %>", { :notice => "<%= singular_table_name.humanize %> deleted successfully."} )    
 <% end -%>
 <% end -%>
   end
